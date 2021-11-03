6ebebc9e7c4beb6f35095372c48caf41d973559d
grantjensen <gjensen6@wisc.edu>
2021-11-03 16:47:17 -0500

    Made ugly version of record view with stop/record button and a prompt as to whether you wish to save the audio or not.

--- convey/Shared/Data/ViewModel/RecordViewModel.swift
+++ convey/Shared/Data/ViewModel/RecordViewModel.swift
@@ -13,6 +13,13 @@ class RecordViewModel : ObservableObject {
     private var firestoreService : FirestoreService
     private var authService : AuthService
     
+    @Published var isRecording = false
+    @Published var saveRecording = 0
+    //0: no recording to look at.
+    //1: recording to look at.
+    //2: delete recording.
+    //3: save recording
+    
     init(firestoreService : FirestoreService, authService : AuthService) {
         self.firestoreService = firestoreService
         self.authService = authService
--- convey/Shared/UI/Authed/RecordView.swift
+++ convey/Shared/UI/Authed/RecordView.swift
@@ -11,19 +11,80 @@ struct RecordView : View {
     
     @ObservedObject private var viewModel = ViewModelModule.passRecordViewModel()
     
+    var recordButton : some View {
+        Button(action: {
+            viewModel.isRecording = true
+            viewModel.saveRecording = 0
+        }) {
+            Text("Record")
+                .font(.system(size: 25))
+        }.foregroundColor(Color.black)
+        .padding(30)
+        .background(
+            Circle()
+            .fill(Color.red)
+        )
+        
+    }
+    
+    var savePopup : some View {
+        HStack {
+            //Save
+            Button(action: {
+                viewModel.saveRecording = 3
+            }) {
+                Text("Save")
+            }.padding(30)
+            
+            //Delete
+            Button(action: {
+                viewModel.saveRecording = 2
+            }) {
+                Text("Delete")
+            }.padding(30)
+        }
+    }
+    
+    
+    var stopButton : some View {
+        Button(action: {
+            viewModel.isRecording = false
+            viewModel.saveRecording = 1
+        }) {
+            Text("Stop")
+                .font(.system(size: 25))
+        }.foregroundColor(Color.black)
+        .padding(30)
+        .background(Color.red)
+    }
+    
+    
     var body: some View {
         
         ZStack {
             
-            Button(action: {
-                viewModel.signOut()
-            }, label: {
-                Text("Sign Out")
+            VStack {
+                Button(action: {
+                    viewModel.signOut()
+                }, label: {
+                    Text("Sign Out")
+                    
+                })
+                if viewModel.saveRecording == 1 {
+                    savePopup
+                }
+                else {
+                    if viewModel.isRecording {
+                        stopButton
+                    } else {
+                        recordButton
+                    }
+                }
                 
-            })
+                
+                // This view just needs a convey
             
-            
-            // This view just needs a convey
+            }
             
             
         }
